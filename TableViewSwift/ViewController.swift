237e62d0d3912c659af9c0b4fd8c40a9cb007bfb
Deep_Kapadia <kapadia.deep12@gmail.com>
2019-01-18 10:46:57 +0530

    TableView_Swift_Sample
    Tableview With Image and Text Basic Sample with Cell.

--- TableViewSwift/ViewController.swift
+++ TableViewSwift/ViewController.swift
@@ -8,10 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    var arr:[String]? = nil
+    var img:[String]? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        arr = ["apple","iphone","mac","ios","ipad","ipod"]
+        img = ["image-1.jpg","image-1.jpg","image-1.jpg","image-1.jpg","image-1.jpg","image-1.jpg"]
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +26,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return arr!.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell=tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = arr?[indexPath.row]
+        cell.imageView?.image = UIImage(named: (img?[indexPath.row])!)
+        return cell
+    }
+    
+    
 
 }
 
